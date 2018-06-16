dc1972287baafd2483d0e341b281f721e6fa299b
Luiz Reis <luzrei@hotmail.com>
2018-06-16 15:04:02 -0300

    refactor(Main.hs): init server on port 3003

--- trafic-simulator-api/src/Main.hs
+++ trafic-simulator-api/src/Main.hs
@@ -1,10 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Web.Scotty
+import Config.Routes
 
 main = do
-    putStrLn "Starting Server..."
-    scotty 3000 $ do
-        get "/hello" $ do
-            text "hello world!"
+  putStrLn "Starting Server..."
+  scotty 3003 routes
