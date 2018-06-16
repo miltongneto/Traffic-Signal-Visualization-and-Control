66acc19c30aa89ca8b56e3c9ce3f85d9d91697b3
Luiz Reis <luzrei@hotmail.com>
2018-06-16 15:01:33 -0300

    feat(Routes.hs): add routes module

---
+++ trafic-simulator-api/src/config/Routes.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Config.Routes where
+
+import Web.Scotty
+import Controller.TrafficSignalController as TSC
+
+routes :: ScottyM ()
+routes = do 
+  get "/api/traffic-signal" TSC.getAll
+  get "/api/traffic-signal/:id" TSC.getById
+  post "/api/traffic-signal/:id" TSC.updateById
